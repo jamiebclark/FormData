fa4cfb7cadeb7eea259e2ed6423095ca709e0de7
Jamie Desktop <jamiebclark@gmail.com>
2017-03-12 13:28:28 -0400

    Added paramters to SluggableBehavior and option to make slugs unique

--- Model/Behavior/SluggableBehavior.php
+++ Model/Behavior/SluggableBehavior.php
@@ -6,22 +6,41 @@ class SluggableBehavior extends ModelBehavior {
 
 	private $slugField = 'slug';
 
+	public function setup(Model $Model, $settings = []) {
+		$default = [
+			'unique' => false,
+			'slugField' => 'slug',
+			'displayField' => $Model->displayField,
+		];
+
+		if (!isset($this->settings[$Model->alias])) {
+			$this->settings[$Model->alias] = $default;
+		}
+		$this->settings[$Model->alias] = array_merge($this->settings[$Model->alias], (array) $settings);
+		return parent::setup($Model, $settings);
+	}
+
 	public function beforeFind(Model $Model, $query = array()) {
+		$settings = $this->settings[$Model->alias];
+
+		// Searches by either the slug or the id
 		if (!empty($query['slug'])) {
 			if (is_numeric($query['slug'])) {
 				$query['conditions'][$Model->primaryKey] = $query['slug'];
 			} else {
-				$query['conditions'][$Model->escapeField($this->slugField)] = $query['slug'];
+				$query['conditions'][$Model->escapeField($settings['slugField'])] = $query['slug'];
 			}
 			unset($query['slug']);
 			return $query;
 		}
+
 		return parent::beforeFind($Model, $query);
 	}
 
 	public function beforeSave(Model $Model, $options = array()) {
-		if (!empty($Model->data[$Model->alias][$Model->displayField])) {
-			$Model->data[$Model->alias][$this->slugField] = $this->inflectSlug($Model->data[$Model->alias][$Model->displayField]);
+		$settings = $this->settings[$Model->alias];
+		if (!empty($Model->data[$Model->alias][$settings['displayField']])) {
+			$Model->data[$Model->alias][$settings['slugField']] = $this->inflectSlug($Model->data[$Model->alias][$settings['displayField']]);
 		}
 		return parent::beforeSave($Model, $options);
 	}
@@ -31,8 +50,9 @@ class SluggableBehavior extends ModelBehavior {
  *
  * @return string The slug field
  **/
-	public function getSluggableField() {
-		return $this->slugField;
+	public function getSluggableField(Model $Model) {
+		$settings = $this->settings[$Model->alias];
+		return $settings['slugField'];
 	}
 
 /**
@@ -43,10 +63,11 @@ class SluggableBehavior extends ModelBehavior {
  * @return bool on success;
  **/
 	public function setSlug(Model $Model, $id) {
-		$result = $Model->read(array($Model->displayField), $id);
+		$settings = $this->settings[$Model->alias];
+		$slug = $this->getSlug($Model, $id);
 		return $Model->save(array(
 			$Model->primaryKey => $id,
-			$this->slugField => $this->inflectSlug($result[$Model->alias][$Model->displayField]),
+			$settings['slugField'] => $slug,
 		), array('callbacks' => false, 'validate' => false));
 	}
 
@@ -57,17 +78,53 @@ class SluggableBehavior extends ModelBehavior {
  * @return int The number of affected rows
  **/
 	public function resetAllSlugs(Model $Model) {
+		$settings = $this->settings[$Model->alias];
+		$slugField = $Model->escapeField($settings['slugField']);
 		$result = $Model->find('all', array(
 			'fields' => array($Model->escapeField()),
-			'conditions' => array(
-				$Model->escapeField($this->slugField) => array('', null),
-			)
+			'conditions' => [
+				'OR' => [
+					[$slugField => ""],
+					[$slugField => null]
+				]
+			]
 		));
 		foreach ($result as $row) {
 			$Model->create();
 			$this->setSlug($Model, $row[$Model->alias][$Model->primaryKey]);
 		}
 		return count($result);
+	}
+
+/**
+ * Finds a slug based on the model row values
+ *
+ * @param Model $Model The model obejct
+ * @param int $id The id of the specified row
+ * @return string The newly created slug value
+ **/
+	public function getSlug(Model $Model, $id) {
+		$settings = $this->settings[$Model->alias];
+		$result = $Model->read([$settings['displayField']], $id);
+		$slug = $this->inflectSlug($result[$Model->alias][$settings['displayField']]);
+
+		// Ensures the slug doesn't already exist
+		if (!empty($settings['unique'])) {
+			$slugCount = 0;
+			$newSlug = $slug;
+			while ($result = $Model->find('first', [
+				'recursive' => -1,
+				'conditions' => [
+					'NOT' => [$Model->escapeField() => $id],
+					$Model->escapeField($settings['slugField']) => $newSlug,
+				],
+				'limit' => 1,
+			])) {
+				$newSlug = $slug . '_' . ($slugCount++);
+			}
+			$slug = $newSlug;
+		}
+		return $slug;
 	}
 
 /**
